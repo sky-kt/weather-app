31dab12c03e2b7999f140ba04e26b416eeeb6b07
sky-kt <skyler.kt@protonmail.com>
2021-07-18 08:56:24 -0700

    change API request to be weekly

--- src/index.js
+++ src/index.js
@@ -1,35 +1,18 @@
 const makeApiRequests = async () => {
   try {
-    // make API request to forecast London weather
-    const locationLink = 'http://api.openweathermap.org/data/2.5/forecast/?q=Cupertino&units=imperial&appid=daab30e51d1d719de2096678f035d4bf'
-    // wait for all APIs to come back
-    const locationPromise = await Promise.all([fetch(locationLink, { mode: 'cors' })])
-    const locationJson = locationPromise[0].json()
-
-    // finally view forecast
-    locationJson.then((finalPromise) => {
-      // console.log(finalPromise)
-      const rawArrays = finalPromise.list
-      const organizedArrays = []
-      for (const info in rawArrays) {
-        // A) Time array must have values.
-        // B) The times have to be the same.
-        if (organizedArrays[0] && rawArrays[info].dt_txt.slice(0, 10) === organizedArrays[organizedArrays.length - 1][0].dt_txt.slice(0, 10)) {
-          organizedArrays[organizedArrays.length - 1].push(rawArrays[info])
-        } else {
-          organizedArrays.push([rawArrays[info]])
-        }
-      }
-      console.log('organizedArrays', organizedArrays)
-      averageInfo(organizedArrays)
+    // make API request to forecast weather (7 days)
+    const lat = 37.3229978; const lon = -122.0321823
+    const locationLink = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=current,hourly,minutely,alerts&units=imperial&appid=daab30e51d1d719de2096678f035d4bf`
+    const locationPromise = await fetch(locationLink, { mode: 'cors' })
+    const locationJSON = locationPromise.json()
+    // act upon data
+    locationJSON.then((data) => {
+      console.log(data)
     })
+    // once returned, act on promise
   } catch (err) {
     alert(err)
   }
-}
-
-const averageInfo = (dayArrays) => {
-  console.log('averaging info now...')
 }
 
 makeApiRequests()
