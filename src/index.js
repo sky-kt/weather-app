9bf3fbf7226e1fc096386883dfe6a52e93ac4f7b
sky-kt <skyler.kt@protonmail.com>
2021-07-18 09:24:31 -0700

    findExtremes can find data (now)

--- src/index.js
+++ src/index.js
@@ -1,18 +1,36 @@
 const makeApiRequests = async () => {
   try {
-    // make API request to forecast weather (7 days)
     const lat = 37.3229978; const lon = -122.0321823
+
+    // make API request to forecast weather (7 days)
     const locationLink = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=current,hourly,minutely,alerts&units=imperial&appid=daab30e51d1d719de2096678f035d4bf`
     const locationPromise = await fetch(locationLink, { mode: 'cors' })
     const locationJSON = locationPromise.json()
+
     // act upon data
     locationJSON.then((data) => {
       console.log(data)
+      findExtremes(data)
     })
-    // once returned, act on promise
   } catch (err) {
     alert(err)
   }
+}
+
+const findExtremes = (data) => {
+  const weekArray = data.daily
+  console.log(weekArray)
+  const organizedArray = []
+  for (const day in weekArray) {
+    organizedArray.push({
+      day,
+      dayTemp: weekArray[day].temp.day,
+      nightTemp: weekArray[day].temp.night,
+      weatherVag: weekArray[day].weather[0].main,
+      weatherDes: weekArray[day].weather[0].description
+    })
+  }
+  console.log(organizedArray)
 }
 
 makeApiRequests()
